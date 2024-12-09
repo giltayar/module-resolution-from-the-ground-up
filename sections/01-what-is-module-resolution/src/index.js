5e6f39774351dff2a3e81f9a1b37b34016467e5f
Gil Tayar <gil@tayar.org>
2024-12-09 18:42:31 +0200

    moved iterations to 5

--- sections/01-what-is-module-resolution/src/index.js
+++ sections/01-what-is-module-resolution/src/index.js
@@ -3,6 +3,6 @@ import {show} from './show.js'
 
 const throttledShow = throttle({interval: 500, limit: 1})(show)
 
-for (const _ of Array.from({length: 10})) {
+for (const _ of Array.from({length: 5})) {
   throttledShow('hello world')
 }

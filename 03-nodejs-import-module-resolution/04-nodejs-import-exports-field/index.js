3894ef865ce6933a9f626ab3db8e7831ade556a5
Gil Tayar <gil@tayar.org>
2024-12-12 18:53:24 +0200

    03 04 done

---
+++ 03-nodejs-import-module-resolution/04-nodejs-import-exports-field/index.js
@@ -0,0 +1,7 @@
+import {hello, world} from 'hello'
+
+import {hello as hello2} from 'hello-multiple'
+import {world as world2} from 'hello-multiple/world'
+
+console.log(hello, world)
+console.log(hello2, world2)

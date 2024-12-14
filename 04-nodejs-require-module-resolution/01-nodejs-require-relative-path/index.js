a656770f26bec17cc66ee144ead87c5887573d3a
Gil Tayar <gil@tayar.org>
2024-12-14 17:32:57 +0200

    04 01 added

---
+++ 04-nodejs-require-module-resolution/01-nodejs-require-relative-path/index.js
@@ -0,0 +1,6 @@
+const {hello} = require('./say/hello.js')
+const {world} = require('./say/world')
+const {wink} = require('./say-no-more')
+
+console.log(hello, world)
+console.log(wink)
---
+++ 04-nodejs-require-module-resolution/01-nodejs-require-relative-path/say-no-more/say-no-more.js
@@ -0,0 +1,1 @@
+exports.wink = 'wink 😉';

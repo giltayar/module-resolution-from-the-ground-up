8eebba07bc6e5d463b23c3585795b4fa2c302bd9
Gil Tayar <gil@tayar.org>
2025-01-06 20:58:59 +0200

    test passes with ftgu

--- 02-browser-module-resolution/01-browser-relative-path-exercises/01-test/test.spec.js
+++ 02-browser-module-resolution/01-browser-relative-path-exercises/01-test/test.spec.js
@@ -1,15 +1,7 @@
 import {test, expect} from '@playwright/test'
-import {exec} from 'node:child_process'
+import {exerciseDirectory, prepareTest} from '@giltayar/ftgu/prepare-browser-test'
 
-const solution = process.env.SOLUTION
-const exerciseNumber = new URL('.', import.meta.url).href.split('/').at(-2)?.replace('-test', '')
-const exerciseDirectory = solution ? `../${exerciseNumber}-${solution}` : `../${exerciseNumber}`
-
-test.beforeAll(async () => {
-  exec(`pnpm install && pnpm start`, {cwd: exerciseDirectory})
-
-  await expect(() => fetch('http://localhost:3000')).toPass()
-})
+prepareTest(exerciseDirectory(import.meta.url), test, expect)
 
 test('test', async ({page}) => {
   await page.goto('http://localhost:3000/')
